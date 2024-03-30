a24be441fbf785fcf02640939e6bb93f65db390f
Hassan El-Amin <hello@hassanelamin.co>
2024-03-29 23:31:14 -0700

    Uploadfile (#1)
    * setup supabase/prisma
    
    * setup supabase/prisma
    
    * implemented upload file feature
    
    * implemented upload file feature

---
+++ src/components/handleFileUpload.tsx
@@ -0,0 +1,35 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+
+interface SerializedFile {
+  name: string;
+  format: string;
+  size: number;
+  url: string;
+}
+
+export async function handleFileUpload(serializedFile: SerializedFile) {
+  try {
+    // Fetch the content of the file from the URL
+    const response = await fetch(serializedFile.url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch the file from ${serializedFile.url}`);
+    }
+    const contentBuffer = await response.arrayBuffer(); // Get the file content as ArrayBuffer
+
+    const fileData = await prisma.file.create({
+      data: {
+        name: serializedFile.name,
+        format: serializedFile.format,
+        size: serializedFile.size,
+        content: Buffer.from(contentBuffer),
+      },
+    });
+
+    return fileData;
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    throw error;
+  }
+}
